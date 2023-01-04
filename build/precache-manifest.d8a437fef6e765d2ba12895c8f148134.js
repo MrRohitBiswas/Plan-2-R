3bc5b8fe584dad20ae72be08480de329bd39f4ff
Rohit Biswas <51377066+MrRohitBiswas@users.noreply.github.com>
2023-01-04 17:50:26 +0530

    Add files via upload

---
+++ build/precache-manifest.d8a437fef6e765d2ba12895c8f148134.js
@@ -0,0 +1,198 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98856ccfedc60922337acd9e23ba4bfc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54704dd1c0fddb42aa25",
+    "url": "/static/css/main.551e7ec6.chunk.css"
+  },
+  {
+    "revision": "4b66f4a4b325e1dcab8a",
+    "url": "/static/js/2.86acc8d9.chunk.js"
+  },
+  {
+    "revision": "b485ca6bfc6a3fdcfcd62a4d108892b6",
+    "url": "/static/js/2.86acc8d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54704dd1c0fddb42aa25",
+    "url": "/static/js/main.ceed228a.chunk.js"
+  },
+  {
+    "revision": "03c30e8ce210cfde773f",
+    "url": "/static/js/runtime-main.40345808.js"
+  },
+  {
+    "revision": "e8ed4e39e4d749001a3ffffb09261255",
+    "url": "/static/media/ArunachalPradesh.e8ed4e39.jpg"
+  },
+  {
+    "revision": "708f02dd5bb33141ec7b6acc7edd31ec",
+    "url": "/static/media/Assam.708f02dd.jpg"
+  },
+  {
+    "revision": "56a0397f75f1369959cda58986f924e0",
+    "url": "/static/media/Bihar.56a0397f.jpg"
+  },
+  {
+    "revision": "422c592a162d82275d4e69090dc1021b",
+    "url": "/static/media/Delhi.422c592a.jpg"
+  },
+  {
+    "revision": "41227c1ac6a718de534d8114317764f8",
+    "url": "/static/media/Goa.41227c1a.jpg"
+  },
+  {
+    "revision": "ed5a5b53f2338a8427d3a415943d96b8",
+    "url": "/static/media/Gujarat.ed5a5b53.jpg"
+  },
+  {
+    "revision": "7dbaaf616bfe76ce0cb9752ca3aabd99",
+    "url": "/static/media/HimachalPradesh.7dbaaf61.jpg"
+  },
+  {
+    "revision": "fdd9332498ca812a748ca84223d6dcfa",
+    "url": "/static/media/JammuAndKashmir.fdd93324.jpg"
+  },
+  {
+    "revision": "d8c292848e346323501082cdda55a72e",
+    "url": "/static/media/Karnataka.d8c29284.jpg"
+  },
+  {
+    "revision": "913086cb1d3f5aa1cf3da79ecee8a3ae",
+    "url": "/static/media/Kerala.913086cb.jpg"
+  },
+  {
+    "revision": "e8d6b85e14f98fcded0e25c4298ef2be",
+    "url": "/static/media/Maharashtra.e8d6b85e.jpg"
+  },
+  {
+    "revision": "7ef36b54d6984729f71d3e04f978cf45",
+    "url": "/static/media/Manipur.7ef36b54.jpg"
+  },
+  {
+    "revision": "46a5952ee1b4b597e6b03adabb483ba0",
+    "url": "/static/media/Meghalaya.46a5952e.jpg"
+  },
+  {
+    "revision": "f2cf584d8f063283a3f885d0eafced2a",
+    "url": "/static/media/Mizoram.f2cf584d.jpg"
+  },
+  {
+    "revision": "dc9b942e392f620431564b347674d7af",
+    "url": "/static/media/Nagaland.dc9b942e.jpg"
+  },
+  {
+    "revision": "523577b4a62f10dc969c289477365aef",
+    "url": "/static/media/Punjab.523577b4.jpg"
+  },
+  {
+    "revision": "36ebf004e903db612388510a379aea28",
+    "url": "/static/media/Rajasthan.36ebf004.jpg"
+  },
+  {
+    "revision": "be8bd91a31d136543772cf6b84f3e8a1",
+    "url": "/static/media/Sikkim.be8bd91a.jpg"
+  },
+  {
+    "revision": "883382b917fc11a4cf8abe147d0c644a",
+    "url": "/static/media/TamilNadu.883382b9.jpg"
+  },
+  {
+    "revision": "392e1966f2e19e5ab9b8206f6a4cba39",
+    "url": "/static/media/Tripura.392e1966.jpg"
+  },
+  {
+    "revision": "2cbe8fc9dee39992c80e0d68d586cbe0",
+    "url": "/static/media/UttarPradesh.2cbe8fc9.jpg"
+  },
+  {
+    "revision": "ecc444cd76c76fad47713741b5f9199c",
+    "url": "/static/media/Uttarakhand.ecc444cd.jpg"
+  },
+  {
+    "revision": "fc0b68f7c8bf7d7707ed04ba66cbd983",
+    "url": "/static/media/WestBengal.fc0b68f7.jpg"
+  },
+  {
+    "revision": "2d695e7071476fbb9a68cb50bb638a6a",
+    "url": "/static/media/bg-temp.2d695e70.jpg"
+  },
+  {
+    "revision": "87a40cef8d7b4b6167171f51b7492a74",
+    "url": "/static/media/bg.87a40cef.jpg"
+  },
+  {
+    "revision": "0cad3d8ca8ec12ae66461ec1bb7325e7",
+    "url": "/static/media/bg1.0cad3d8c.jpg"
+  },
+  {
+    "revision": "c7c7890e21abfaf5bcb3a0db53925e5c",
+    "url": "/static/media/bg2.c7c7890e.jpg"
+  },
+  {
+    "revision": "75c5262bd4ac7e2f31cf0f57a98a0898",
+    "url": "/static/media/bg3.75c5262b.jpg"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "2679909f34a0d6dc3db467802d6bc2c1",
+    "url": "/static/media/fav.2679909f.png"
+  },
+  {
+    "revision": "0f9928d77bc67692e90527ec092fcbd0",
+    "url": "/static/media/feature-tile-icon-01.0f9928d7.svg"
+  },
+  {
+    "revision": "bd39f30434175e7c8b1d1c74fa6569fd",
+    "url": "/static/media/feature-tile-icon-02.bd39f304.svg"
+  },
+  {
+    "revision": "66f37ba5c7e4b6648f04883fa637e634",
+    "url": "/static/media/feature-tile-icon-03.66f37ba5.svg"
+  },
+  {
+    "revision": "836acd100a942420f8ab4e6c5e4162d8",
+    "url": "/static/media/feature-tile-icon-04.836acd10.svg"
+  },
+  {
+    "revision": "fa9ba00b178abc7bd2e02bd986e91fe3",
+    "url": "/static/media/feature-tile-icon-05.fa9ba00b.svg"
+  },
+  {
+    "revision": "6a1776963deb39d710bae80bb449aebe",
+    "url": "/static/media/feature-tile-icon-06.6a177696.svg"
+  },
+  {
+    "revision": "c46278a25d28ceb6cf844751bf1c36d0",
+    "url": "/static/media/features-split-image-01.c46278a2.png"
+  },
+  {
+    "revision": "06d71487cdb2a17aee12c411abffa8d3",
+    "url": "/static/media/features-split-image-02.06d71487.png"
+  },
+  {
+    "revision": "63f1e8082aaa3f31b576989ddf603925",
+    "url": "/static/media/features-split-image-03.63f1e808.png"
+  },
+  {
+    "revision": "b0b1342557dd4e63b82dde683bd1f0f7",
+    "url": "/static/media/illustration-section-01.b0b13425.png"
+  },
+  {
+    "revision": "eaef9924b2bc451747b397aa3f24807e",
+    "url": "/static/media/illustration-section-01.eaef9924.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "d55a4fa46d26dbede786d09412a403eb",
+    "url": "/static/media/thumbnail.d55a4fa4.jpg"
+  }
+]);
