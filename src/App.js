0a2077f5f021fb46d4b2ab694061fe06563778d0
Rohit Biswas <51377066+MrRohitBiswas@users.noreply.github.com>
2023-01-16 02:04:53 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import {  Switch} from 'react-router-dom';
+import AppRoute from './utils/AppRoute';
+import Navbar from "./components/Navbar";
+import Footer from './components/Footer';
+import Services from './components/Services'
+import LogReg from './components/LogReg';
+import SearchState from './components/SearchState';
+import HotelDetails from './components/topPlaces/hotels/HotelDetails';
+import PlaceDetails from './components/topPlaces/PlaceDetailsComponents/PlaceDetails';
+
+// Layouts
+import LayoutDefault from './layouts/LayoutDefault';
+// Views 
+
+import ContactUsEntry from './ContactUsEntry';
+//Helmet
+import { Helmet } from 'react-helmet';
+// Initialize Google Analytics
+import ReactGA from 'react-ga';
+const msr_iD ="G-1Y51BD6TYZ"; //measurement id
+ReactGA.initialize(msr_iD);
+
+
+
+const App = () => {
+
+  return (
+    <>
+      <Helmet>
+        <title>Tour CirKit | Homepage</title>
+        <meta name='Homepage of TourCirKit' content='Get started with the experience of all new unique tour planning app'/>
+      </Helmet>
+     <Navbar title="Tour CirKit" />
+        <Switch>
+          <AppRoute exact path="/" component={Services} />
+
+          <AppRoute exact path="/Contact" component={ContactUsEntry} layout={LayoutDefault} />
+          <AppRoute exact path="/Services" component={Services}/>
+          <AppRoute exact path="/LogReg" component={LogReg} layout={LayoutDefault} />
+          <AppRoute exact path="/SearchState/:id" component={SearchState} layout={LayoutDefault}/>
+          <AppRoute exact path="/HotelDetails/:id" component={HotelDetails}/>
+          <AppRoute exact path="/route/:id" component={PlaceDetails}/>
+
+          
+        </Switch>
+        <Footer/>
+      </>
+ 
+  );
+}
+
+export default App;
