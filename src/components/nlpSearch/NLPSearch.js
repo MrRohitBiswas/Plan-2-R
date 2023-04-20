1f9e988225d9aa552a8758364c667261e80d7d98
Rohit Biswas <rohitbiswas31012001@gmail.com>
2023-04-20 13:08:54 +0530

    completed nlp with css

--- src/components/nlpSearch/NLPSearch.js
+++ src/components/nlpSearch/NLPSearch.js
@@ -3,28 +3,31 @@ import Chart from '../stats/Chart'
 import SearchBar from './SearchBar'
 import { Helmet } from "react-helmet";
 import ReactGA from "react-ga";
-
+import './NLPSearch.css'
 export default function NLPSearch() {
 
    //GA
    useEffect(() => {
     ReactGA.pageview(window.location.pathname); //interaction event
   });
-
+  
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
   return (
+    <div className='nlpsearchmain'>
+
     <div className="container">
        <Helmet>
         <title>Tour CirKit | NLP Search</title>
         <meta
-          name="Services Provided"
-          content="All services provided by us, browse states and search wherever you plan to travel"
+          name="Advcanced Search Mechanism"
+          content="Search in your own language and express, we will find the place for you"
         />
       </Helmet>
         <SearchBar/>
         <Chart/>
+    </div>
     </div>
   )
 }
--- src/components/nlpSearch/Resp/Resp.js
+++ src/components/nlpSearch/Resp/Resp.js
@@ -1,28 +1,29 @@
-import React, { useEffect, useState } from 'react'
-import {useLocation} from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { useLocation, Link } from "react-router-dom";
+import "./Resp.scss";
 import './Resp.css'
+import logo from '../../../components/components/resImg.jpg'
 
 export default function Resp(loc) {
-  
   const location = useLocation();
   const keyword = location.pathname.replace("/search/", "");
   const url = `/search2?id=${encodeURIComponent(keyword)}`;
   const [isLoading, setIsLoading] = useState(false);
   const [res, setRes] = useState([]);
-    
+
   useEffect(() => {
     if (keyword) {
       async function fetchData() {
         setIsLoading(true);
         try {
           const options = {
-            method: 'GET',
-            mode: 'no-cors'
+            method: "GET",
+            mode: "no-cors",
           };
           const response = await fetch(url, options);
           const jsonData = await response.json();
-          console.log(jsonData);
-          setRes(jsonData);
+          console.log(jsonData.res);
+          setRes(jsonData.res);
         } catch (error) {
           console.error(error);
           setRes([]);
@@ -34,17 +35,46 @@ export default function Resp(loc) {
     } else {
       setRes([]);
     }
-    return setRes([])
+    return setRes([]);
   }, [keyword, url]);
-  
-  
-  
-  
+
   return (
-    <div>
-        <p>
-          {/* {JSON.stringify(res)} */}
-        </p>
+    <div className="nlpRes">
+      <ul className = 'resList'>
+        {isLoading ? (
+          <></>
+        ) : (
+          res.map((spots, indx) => {
+            return (
+              <li className="topPlacesCards" key={indx} style={{margin: 'auto'}}>
+                <div class="nlp">
+                  <div class="main">
+                    <h1 style={{ fontSize: "20px", fontWeight: "400", marginBottom:'0' , lineHeight: '30px', textAlign:'center', marginTop:'26%'}}>
+                      {spots.Place}
+                    </h1>
+                    <p class="description">{spots.City}</p>
+
+                  </div>
+                  <div className="resLinks">
+                    
+                    <p class="resLinksChild lr" >
+                      <Link to={{pathname: `/route/src=${spots.City}&dest=${spots.Place + ','}%20${spots.City}`, imgUrl: `${logo}`}}>
+                        Details
+                        </Link>
+                        </p>
+                    
+                    <p class="resLinksChild rr">
+                      <Link to={`/HotelDetails/${spots.City}`}>
+                    Hotels
+                    </Link>
+                    </p>
+                  </div>
+                </div>
+              </li>
+            );
+          })
+        )}
+      </ul>
     </div>
-  )
+  );
 }
