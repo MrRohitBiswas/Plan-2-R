a58b0cb7e820b242fe9f1e24d042d92b13bb9a1a
Rohit Biswas <rohitbiswas31012001@gmail.com>
2023-05-17 16:07:15 +0530

    dynamicxchatbot

--- build/static/scripts/chat.js
+++ build/static/scripts/chat.js
@@ -1,99 +1,109 @@
 var coll = document.getElementsByClassName("collapsible");
 
 for (let i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function () {
-        this.classList.toggle("active");
+  coll[i].addEventListener("click", function () {
+    this.classList.toggle("active");
 
-        var content = this.nextElementSibling;
+    var content = this.nextElementSibling;
 
-        if (content.style.maxHeight) {
-            content.style.maxHeight = null;
-        } else {
-            content.style.maxHeight = content.scrollHeight + "px";
-        }
-
-    });
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
 }
 
 function getTime() {
-    let today = new Date();
-    hours = today.getHours();
-    minutes = today.getMinutes();
+  let today = new Date();
+  hours = today.getHours();
+  minutes = today.getMinutes();
 
-    if (hours < 10) {
-        hours = "0" + hours;
-    }
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
 
-    if (minutes < 10) {
-        minutes = "0" + minutes;
-    }
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
 
-    let time = hours + ":" + minutes;
-    return time;
+  let time = hours + ":" + minutes;
+  return time;
 }
 
 function firstBotMessage() {
-    let firstMessage = "How's it going?"
-    document.getElementById("botStarterMessage").innerHTML = '<p class="botText"><span>' + firstMessage + '</span></p>';
+  let firstMessage = "How's it going?";
+  document.getElementById("botStarterMessage").innerHTML =
+    '<p class="botText"><span>' + firstMessage + "</span></p>";
 
-    let time = getTime();
+  let time = getTime();
 
-    $("#chat-timestamp").append(time);
-    document.getElementById("userInput").scrollIntoView(false);
+  $("#chat-timestamp").append(time);
+  document.getElementById("userInput").scrollIntoView(false);
 }
 
 firstBotMessage();
 
-function getHardResponse(userText) {
-    let botResponse = getBotResponse(userText);
-    let botHtml = '<p class="botText"><span>' + botResponse + '</span></p>';
-    $("#chatbox").append(botHtml);
+async function getHardResponse(userText) {
+  let botResponse =await getBotResponse(userText);
+  let botHtml = '<p class="botText"><span>' + botResponse + "</span></p>";
+  $("#chatbox").append(botHtml);
 
-    document.getElementById("chat-bar-bottom").scrollIntoView(true);
+  document.getElementById("chat-bar-bottom").scrollIntoView(true);
 }
 
 function getResponse() {
-    let userText = $("#textInput").val();
+  let userText = $("#textInput").val();
 
-    if (userText == "") {
-        let rand = ['I love Tour Cirkit', 'What do you offer?', 'Send me to services page', 'Please connect with me', 'Hill Stations', 'Beaches', 'Historical Monuments', 'Top Places in Darjeeling', 'Thanks!', 'Bye, lots of love!']
-        userText = rand[Math.floor(Math.random() * 10)];
-    }
+  if (userText == "") {
+    let rand = [
+      "I love Tour Cirkit",
+      "What do you offer?",
+      "Send me to services page",
+      "Please connect with me",
+      "Hill Stations",
+      "Beaches",
+      "Historical Monuments",
+      "Top Places in Darjeeling",
+      "Thanks!",
+      "Bye, lots of love!",
+    ];
+    userText = rand[Math.floor(Math.random() * 10)];
+  }
 
-    let userHtml = '<p class="userText"><span>' + userText + '</span></p>';
+  let userHtml = '<p class="userText"><span>' + userText + "</span></p>";
 
-    $("#textInput").val("");
-    $("#chatbox").append(userHtml);
-    document.getElementById("chat-bar-bottom").scrollIntoView(true);
+  $("#textInput").val("");
+  $("#chatbox").append(userHtml);
+  document.getElementById("chat-bar-bottom").scrollIntoView(true);
 
-    setTimeout(() => {
-        getHardResponse(userText);
-    }, 1000)
-
+  setTimeout(() => {
+     getHardResponse(userText);
+  }, 1000);
 }
 
 function buttonSendText(sampleText) {
-    let userHtml = '<p class="userText"><span>' + sampleText + '</span></p>';
+  let userHtml = '<p class="userText"><span>' + sampleText + "</span></p>";
 
-    $("#textInput").val("");
-    $("#chatbox").append(userHtml);
-    document.getElementById("chat-bar-bottom").scrollIntoView(true);
+  $("#textInput").val("");
+  $("#chatbox").append(userHtml);
+  document.getElementById("chat-bar-bottom").scrollIntoView(true);
 
-    setTimeout(() => {
-        getHardResponse(sampleText);
-    }, 1000)
+  setTimeout(() => {
+    getHardResponse(sampleText);
+  }, 1000);
 }
 
 function sendButton() {
-    getResponse();
+  getResponse();
 }
 
 function heartButton() {
-    buttonSendText("&#128150;")
+  buttonSendText("&#128150;");
 }
 
 $("#textInput").keypress(function (e) {
-    if (e.which == 13) {
-        getResponse();
-    }
+  if (e.which == 13) {
+    getResponse();
+  }
 });
--- build/static/scripts/responses.js
+++ build/static/scripts/responses.js
@@ -1,44 +1,91 @@
-function getBotResponse(input) {
-    function sendMail(email){
+async function getBotResponse(input) {
+  function sendMail(email) {
+    fetch(`/sendMail?email=${email}`)
+      .then((response) => {
+        if (response.ok) {
+          return response.text();
+        } else {
+          throw new Error("Error: " + response.status);
+        }
+      })
+      .then((data) => {
+        console.log("Response:", data);
+      })
+      .catch((error) => {
+        console.error("Error:", error.message);
+      });
+  }
+  //https://api.opencagedata.com/geocode/v1/json?q=bhubaneshwar&key=276367a7e7b944e686ed5ba7a767743d
 
-      fetch(`/sendMail?email=${email}`)
-  .then(response => {
-    if (response.ok) {
-      return response.text();
-    } else {
-      throw new Error('Error: ' + response.status);
-    }
-  })
-  .then(data => {
-    console.log('Response:', data);
-  })
-  .catch(error => {
-    console.error('Error:', error.message);
-  });
-
-    }
-
-
-
-
-    if (/love tour cirkit|&#128150;/i.test(input)) {
-        return "We're glad to hear that you love Tour Cirkit! We strive to provide the best travel experiences. How can I assist you further?";
-      }else if (/^(hi|hello|hey|greetings|salut|hola)$/i.test(input)) {
-        return `${input.charAt(0).toUpperCase() + input.slice(1)}! How can I assist you today?`;
-      }else if (/services|service|offer|offerings|advantages|benefits|options|option|programs|program|packages|package|deliver|deliverable|treatment|deliveries|intervention|performance|task|duties|duty|assignment|job|project|engagement|contract|output|caregiving|supervision|attendance|expertise|consultation|advisory|guidance|management|solution/i.test(input)) {
-        return "You can explore our services page on our website to discover the various travel services we offer. Simply visit <a href='https://www.tourcirkit.com/Services'>Services</a> to learn more.";
-      } else if (/contact|connect|get in touch|reach out|get hold of|communicate|help|interact|establish contact|contact us|drop a line|message|give us a shout|get ahold of us|get in reach|get in touch with us|establish communication|establish connection|reach us|assist|reach us out|connect to us/i.test(input)) {
-        return "We value your interest! Please provide your email, and our team will be happy to connect with you shortly. You can also reach out to us at <a href='https://www.tourcirkit.com/Contact'>Contact Us</a> for immediate assistance.";
-      } else if (/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(input)) {
-        sendMail(input);
-        return "You should receive an email from us &#128512;";
-      }else if (/thank|gracias|merci|ty/i.test(input)) {
-        return "You're welcome! If you have any more questions or need further assistance, feel free to ask. Have a great day!";
-      } else if (/bye|tata|ta ta|take care|see you|see ya/i.test(input)) {
-        return "Goodbye! It was a pleasure assisting you. If you need any help in the future, don't hesitate to reach out. Take care and lots of love to you too!";
-      }  else if (/top|best|place|hill|beach|historical|cheap|lovely|view|tour/i.test(input)) {
-        return `Sure! I can help you with that. Please click on this <a href='https://www.tourcirkit.com/search/${input}'>link</a> to navigate.`;
-      }  else {
+  async function hasPlace(ip) {
+    try {
+      const response = await fetch(
+        `https://api.opencagedata.com/geocode/v1/json?q=${ip}&key=276367a7e7b944e686ed5ba7a767743d`
+      );
+      if (!response.ok) {
+        throw new Error("Error: " + response.status);
+      }
+      const data = await response.json();
+      const placeCount = data.total_results;
+      const place = data.results;
+      console.log(place);
+      if (
+        placeCount > 0 ||
+        (place[0].components && place[0].components.city) ||
+        place[0].components.county
+      ) {
+        const city = place[0].components.city || place[0].components.county;
+        console.log("City:", city);
+        // window.location.href = `https://tourcirkit.com/SearchState/${city}`;
+        return `https://tourcirkit.com/SearchState/${city}`;
+      } else {
+        console.log("No city found.");
         return "Try asking something else!";
+      }
+    } catch (error) {
+      console.error("Error:", error.message);
     }
+  }
+
+  if (/love tour cirkit|&#128150;/i.test(input)) {
+    return "We're glad to hear that you love Tour Cirkit! We strive to provide the best travel experiences. How can I assist you further?";
+  }else if (/^(hotel|stay|flat|rent|appartment|room)$/i.test(input)) {
+    return 'Please write the name of the place/city whose hotels you want to enquire, we will provide all details of the place as well as of the hotels';
+  }  else if (/^(hi|hello|hey|greetings|salut|hola)$/i.test(input)) {
+  } else if (input) {
+    const placeUrl = await hasPlace(input.toLowerCase());
+    console.log(placeUrl);
+    let res = `Thank you for your inquiry! We have a wide range of detailed information about various places. Click <a  href='${placeUrl}' style="color:blue;">here</a> to go to your desired location`;
+    if (placeUrl) {
+      return res;
+    }
+    return `${
+      input.charAt(0).toUpperCase() + input.slice(1)
+    }! How can I assist you today?`;
+  } else if (
+    /services|service|offer|offerings|advantages|benefits|options|option|programs|program|packages|package|deliver|deliverable|treatment|deliveries|intervention|performance|task|duties|duty|assignment|job|project|engagement|contract|output|caregiving|supervision|attendance|expertise|consultation|advisory|guidance|management|solution/i.test(
+      input
+    )
+  ) {
+    return "You can explore our services page on our website to discover the various travel services we offer. Simply visit <a href='https://www.tourcirkit.com/Services' style='color:blue;'>Services</a> to learn more.";
+  } else if (
+    /contact|connect|get in touch|reach out|get hold of|communicate|help|interact|establish contact|contact us|drop a line|message|give us a shout|get ahold of us|get in reach|get in touch with us|establish communication|establish connection|reach us|assist|reach us out|connect to us/i.test(
+      input
+    )
+  ) {
+    return "We value your interest! Please provide your email, and our team will be happy to connect with you shortly. You can also reach out to us at <a href='https://www.tourcirkit.com/Contact'>Contact Us</a> for immediate assistance.";
+  } else if (/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(input)) {
+    sendMail(input);
+    return "You should receive an email from us &#128512;";
+  } else if (/thank|gracias|merci|ty/i.test(input)) {
+    return "You're welcome! If you have any more questions or need further assistance, feel free to ask. Have a great day!";
+  } else if (/bye|tata|ta ta|take care|see you|see ya/i.test(input)) {
+    return "Goodbye! It was a pleasure assisting you. If you need any help in the future, don't hesitate to reach out. Take care and lots of love to you too!";
+  } else if (
+    /top|best|place|hill|beach|historical|cheap|lovely|view|tour/i.test(input)
+  ) {
+    return `Sure! I can help you with that. Please click on this <a href='https://www.tourcirkit.com/search/${input}' style="color:blue;">link</a> to navigate.`;
+  } else {
+    return "Try asking something else!";
+  }
 }
